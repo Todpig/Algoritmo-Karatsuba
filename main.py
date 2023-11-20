a53dff2d2a57d7048049e32d7f0f4733889cbcd1
todpig <luccasraffael6@gmail.com>
2023-11-20 15:18:22 -0300

    f

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-def karatsuba(x,y):
+import time
+
+def karatsuba(x, y):
     if x < 10 or y < 10:
-        return x*y
-    n =  max(len(str(x)),len(str(y)))
+        return x * y
+    n = max(len(str(x)), len(str(y)))
     m = n // 2
-    
-    x_high, x_low = divmod(x,10**m)
-    y_high, y_low = divmod(y,10**m)
 
-    z0 = karatsuba(x_low,y_low)
-    z1 = karatsuba((x_low+x_high),(y_low+y_high))
-    z2 = karatsuba(x_high,y_high)
-    return z2 * 10**(2*m) + ((z1-z2-z0) * 10**m) + z0
+    x_high, x_low = divmod(x, 10**m)
+    y_high, y_low = divmod(y, 10**m)
 
-#questao 1
-result = karatsuba(1234, 5678)
-print(result)
-result = karatsuba(11111111,22222222)
-print(result)
-result = karatsuba(12345678,12345678)
-print(result)
+    z0 = karatsuba(x_low, y_low)
+    z1 = karatsuba((x_low + x_high), (y_low + y_high))
+    z2 = karatsuba(x_high, y_high)
+    return z2 * 10**(2*m) + ((z1 - z2 - z0) * 10**m) + z0
 
+def standard_multiplication(x, y):
+    return x * y
 
-#questao 2
-result = karatsuba(123, 456789)
-print(result)
-result = karatsuba(12345,567)
-print(result)
-result = karatsuba(123456,78901234)
-print(result)
+start_time = time.time()
+result_karatsuba = karatsuba(1234, 5678)
+end_time_karatsuba = time.time()
+print(f"Karatsuba result: {result_karatsuba}, Time: {end_time_karatsuba - start_time:.6f} seconds")
+
+start_time = time.time()
+result_standard = standard_multiplication(1234, 5678)
+end_time_standard = time.time()
+print(f"Standard multiplication result: {result_standard}, Time: {end_time_standard - start_time:.6f} seconds")
+
+start_time = time.time()
+result_karatsuba = karatsuba(123, 456789)
+end_time_karatsuba = time.time()
+print(f"Karatsuba result: {result_karatsuba}, Time: {end_time_karatsuba - start_time:.6f} seconds")
+
+start_time = time.time()
+result_standard = standard_multiplication(123, 456789)
+end_time_standard = time.time()
+print(f"Standard multiplication result: {result_standard}, Time: {end_time_standard - start_time:.6f} seconds")
